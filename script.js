620a4df98daab6ed75b79972ff5556dea56a55db
Renato Neckel <renatoneckeldev@gmail.com>
2024-04-17 22:31:05 -0300

    Add arquivos

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  /*if(html.classList.contains("light")) {
+       html.classList.remove('light')
+     } else {
+       html.classList.add('light')
+     } esse codigo significa que o JS está buscando com o comando if la em html se contem a lista "light" e após achal se ela estiver la pode desliga-la ou liga-la.
+     esse é o codigo inteiro, porem existe o comando simplificado para que é:*/
+  html.classList.toggle("light")
+
+  /* função para substituir ou adicionar uma nova imagem*/
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/profile-light.png")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/profile.png")
+  }
+}
